c69297e91a3446138a76371c7fdc2673c6ecbdb1
Michael Rice <michael@michaelrice.org>
2019-06-17 15:26:36 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='drpy',
+    version='0.1',
+    packages=['api', 'fsm', 'fsm.states', 'models'],
+    url='https://github.com/rackn',
+    license='Commercial',
+    author='RackN',
+    author_email='eng@rackn.com',
+    description='Digital Rebar Python'
+)
