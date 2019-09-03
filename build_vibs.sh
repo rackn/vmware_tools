1bb0af7b12ff3fc41f2bf47ec0ca92d47d470206
Greg Althaus <galthaus@austin.rr.com>
2019-09-03 14:27:34 -0500

    Add a build_vibs.sh script for me

---
+++ build_vibs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+export TRAVIS_BUILD_DIR=`pwd`
+cd drpy
+./build.sh $TRAVIS_BUILD_DIR/drpy/stage/payloads/drpy/opt/rackn/drpy
+cd $TRAVIS_BUILD_DIR
+./build_tools/set_version.sh drpy/stage/descriptor.xml
+./build_tools/set_version.sh firewall/stage/descriptor.xml
+docker pull lamw/vibauthor
+docker run -d --name=vibauthor -v $TRAVIS_BUILD_DIR/drpy/stage:/root/drpy/stage -v $TRAVIS_BUILD_DIR/firewall:/root/firewall lamw/vibauthor tail -f /dev/null
+docker exec -it vibauthor /bin/bash -c "cd drpy/ && vibauthor -C -t stage -f"
+mkdir -p $TRAVIS_BUILD_DIR/grd/vibs
+cp $TRAVIS_BUILD_DIR/drpy/stage/*.vib $TRAVIS_BUILD_DIR/grd/vibs
+docker exec -it vibauthor /bin/bash -c "cd firewall/ && vibauthor -C -t stage -f"
+cp $TRAVIS_BUILD_DIR/firewall/stage/*.vib $TRAVIS_BUILD_DIR/grd/vibs
+
